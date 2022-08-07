d37f552c3748fdb536f9919d3bf0fe371335b03b
GabrielPSantana <gabrieldevti@gmail.com>
2022-08-06 23:35:21 -0300

    update_views_models

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-# Create your views here.
+from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_datail_page':True,
+    })
